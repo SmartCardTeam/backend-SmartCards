3ee5414699a307bcd9a2c6a0ee7691a55a7aed7b
joaofm <joaofm@ciandt.com>
2023-10-25 17:49:56 -0300

    Primeiro commit.

---
+++ smartcards/src/main/java/com/smartcards/smartcards/SmartCardsApplication.java
@@ -0,0 +1,13 @@
+package com.smartcards.smartcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCardsApplication.class, args);
+	}
+
+}
---
+++ smartcards/src/test/java/com/smartcards/smartcards/SmartcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcards.smartcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
